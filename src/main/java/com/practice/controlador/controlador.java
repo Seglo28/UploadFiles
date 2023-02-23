71fc8cffa489f7419d3b61567f7d9e559504b292
Gloria Sevilla <gloriasevilla76@yahoo.es>
2023-02-23 11:08:53 -0600

    Actualizacion

--- src/main/java/com/practice/controlador/controlador.java
+++ src/main/java/com/practice/controlador/controlador.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.FilenameFilter;
-import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.FilenameUtils; 
 
 @RestController
 @RequestMapping("/controlador")
