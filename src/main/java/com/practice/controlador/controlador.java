e2192858b3a3eaae8933b8d16c81d292a892bd98
Gloria Sevilla <gloriasevilla76@yahoo.es>
2023-03-03 08:10:34 -0600

    I created RemoveBom method. That method delete BOM in your Microsoft UTF-8 documents.

--- src/main/java/com/practice/controlador/controlador.java
+++ src/main/java/com/practice/controlador/controlador.java
@@ -1,4 +1,5 @@
 package com.practice.controlador;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
--- src/main/java/com/practice/especialidades/CvsDetails.java
+++ src/main/java/com/practice/especialidades/CvsDetails.java
@@ -1,14 +1,20 @@
 package com.practice.especialidades;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.tika.Tika;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -95,59 +101,120 @@ public class CvsDetails {
 	}
 	
 	
-	public List<List<String>> printStuff(MultipartFile document){
+	public List<List<String>> printStuff(MultipartFile document) throws IOException{
 		File csvFile = new File ("c:/upload_java/"+document.getOriginalFilename());
         String line = "";
         String cvsSplitBy = ";";
 
         List<List<String>> data = new ArrayList<>();
 
+        if(this.removeBOM(document)) {
+        }
+
+        System.out.println("PRINT-STUFF: Estoy dentro del WHILE.");
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-        	if(this.removeBOM(csvFile) ) {
-        		while ((line = br.readLine()) != null) {       			
+        		while ((line = br.readLine()) != null) {
                     String[] row = line.split(cvsSplitBy);
-                    for (int i = 0; i < row.length; i++) {                  	
+                    for (int i = 0; i < row.length; i++) {   
                         if (i >= data.size()) {                      	
                             data.add(new ArrayList<String>());
                         }
                         data.get(i).add(row[i]);
                     }
                 }
-        	}else{
-        		while ((line = br.readLine()) != null) {       			
-                    String[] row = line.split(cvsSplitBy);
-                    for (int i = 0; i < row.length; i++) {                  	
-                        if (i >= data.size()) {                      	
-                            data.add(new ArrayList<String>());
-                        }
-                        data.get(i).add(row[i]);
-                    }
-                }
-        	}
+        	System.out.println("--------------------------------");
         } catch (IOException e) {
             e.printStackTrace();
         }
 		return data;
     }
 	
-	 public boolean removeBOM(File inputFile) throws IOException {
+	
+	private static final String UTF8_BOM = "\uFEFF";
+	 public boolean BOMinside(MultipartFile document) throws IOException {
 			
 		 boolean isOk = false;
+		 File inputFile = new File ("c:/upload_java/"+document.getOriginalFilename());
 		 
-	        byte[] fileBytes = new byte[(int) inputFile.length()];
-
-	        FileInputStream inputStream = new FileInputStream(inputFile);
-	        inputStream.read(fileBytes);
-	        inputStream.close();
-
-	        isOk = (fileBytes.length > 2 && (fileBytes[0] & 0xFF) == 0xEF && (fileBytes[1] & 0xFF) == 0xBB && (fileBytes[2] & 0xFF) == 0xBF) ? true : false;
-	            byte[] trimmedBytes = new byte[fileBytes.length - 3];
-	            System.arraycopy(fileBytes, 3, trimmedBytes, 0, trimmedBytes.length);
-
-	            FileOutputStream outputStream = new FileOutputStream(inputFile);
-	            outputStream.write(trimmedBytes);
-	            outputStream.close();
-				return isOk;
+		 this.isBoom(inputFile);
+				return this.isBoom(inputFile);
 	    }
+	 
+	 private boolean isBoom(File file) {
+		 boolean isOk = false;
+		 if(!file.exists()) {
+			 System.out.println("METODO IS BOOM? " + "El archivo no existe");
+			 return false;
+		 }
+		 
+		 byte[] BOM = new byte[3];
+		 
+		 try (FileInputStream input = new FileInputStream(file)) {
+			 input.read(BOM);
+			 input.close();
+			 
+			 
+			 String content = new String(Hex.encodeHex(BOM));
+			 System.out.println(file.getName()+ " hex "+content);
+	          if ("efbbbf".equalsIgnoreCase(content)) {
+	              isOk = true;
+	          } else {
+	        	  isOk = false;
+	          }
+	          System.out.println("METODO IS BOOM? " +"isOK "+ isOk);
+			 System.out.println("METODO IS BOOM? " +"el valor de bom es: " + BOM.toString());
+			 
+			 
+		 } catch (Exception e) {
+			 System.out.println("METODO IS BOOM? " +"a ocurrido un error " + BOM);
+			 isOk = false;
+		}
+		 return isOk;
+	 }
+	 
+	 private boolean removeBOM (MultipartFile document) throws IOException {
+		 Path path = Paths.get("c:/upload_java/"+document.getOriginalFilename());
+		 
+		 if(this.BOMinside(document)) {
+			 byte[] bytes = Files.readAllBytes(path);
+			 
+			 ByteBuffer bb = ByteBuffer.wrap(bytes);
+			 System.out.println("ELIMINAR EL BOM " +"Encontramos el BOM en el metodo eliminar");
+			 
+			 byte[] bom = new byte[3];
+			 
+			 bb.get(bom, 0, bom.length);
+			 
+			 byte[] contentAfterFirst3Bytes = new byte[bytes.length - 3];
+	          bb.get(contentAfterFirst3Bytes, 0, contentAfterFirst3Bytes.length);
+	          System.out.println("ELIMINAR EL BOM " +"Removimos el DOM!");
+	          
+	          Files.write(path, contentAfterFirst3Bytes);
+		 } else {
+	          System.out.println("ELIMINAR EL BOM " +"This file doesn't contains UTF-8 BOM!");
+	      }
+		return false;
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 }
-
